1df46512149afef7db4cfd5a2e0a0ed544f7498c
khushab <khushabalam7@gmail.com>
2021-07-06 11:53:43 +0530

    query string

--- src/app.js
+++ src/app.js
@@ -39,8 +39,28 @@ app.get('/help', (req, res) => {
 });
 
 app.get('/weather', (req, res) => {
-  res.send("weatheer page");
+  if (!req.query.address) {
+    return res.send({
+      error: "Please provide an address",
+    })
+  }
+  res.send({
+    forecast: "It is snowing",
+    location: "Philadelphia",
+    address: req.query.address,
+  })
 });
+
+app.get('/products', (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: "Please provide search query"
+    })
+  }
+  res.send({
+    products: [],
+  })
+})
 
 app.get('/help/*', (req, res) => {
   res.render('404', {
